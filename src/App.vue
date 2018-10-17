3613065705cfc753d68c20b42db3ecb395249327
Fabian Muscariello <nroi@mailbox.org>
2018-10-17 10:59:22 +0200

    Rm unused stuff

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <v-app>
-      <img alt="Vue logo" src="./assets/logo.png">
-      <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+      <HelloWorld/>
     </v-app>
   </div>
 </template>
