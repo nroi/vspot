96fbb7466aefb39f96c868d7c8c1b95433717fb7
Fabian Muscariello <nroi@mailbox.org>
2018-10-21 12:24:20 +0200

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
 <template>
   <div id="app">
     <v-app>
-      <ul v-if="$store.state.songs">
-        <li v-for="song in $store.state.songs">
-          <p>{{song.artist}} {{ song.title }}</p>
-        </li>
-      </ul>
+      <v-btn icon>
+        <v-icon>search</v-icon>
+      </v-btn>
+
+      <v-list v-if="$store.state.songs">
+        <template v-for="(song, index) in $store.state.songs">
+          <v-list-tile :key="song.id" @click="">
+            <v-list-tile-action>
+              <v-icon v-if="$store.state.currentSong.id === song.id && $store.state.currentStatus.state === 'play'" color="black">
+                play_circle_outline
+              </v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title v-text="song.title"></v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-divider v-if="index + 1 < $store.state.songs.length" :key="`divider-${index}`"></v-divider>
+        </template>
+      </v-list>
       <SongInfo v-if="$store.state.currentSong" />
       <HelloWorld/>
     </v-app>
@@ -16,7 +30,7 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from './components/HelloWorld.vue';
 import SongInfo from './components/SongInfo.vue';
-import {HEARTBEAT_INTERVAL, UPDATE_INTERVAL} from "./shared";
+import {HEARTBEAT_INTERVAL, UPDATE_INTERVAL} from './shared';
 
 @Component({
   components: {
