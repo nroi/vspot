a3153ff8747274a4632a57bd50430358fdc12a8c
Fabian Muscariello <nroi@mailbox.org>
2018-10-23 21:01:31 +0200

    Put everything inside <v-content>

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,37 @@
 <template>
   <div id="app">
     <v-app>
-      <v-text-field
-              v-model="$store.state.filterInput"
-              prepend-icon="search"
-              box
-              solo
-              clearable
-              label="Filter"
-              type="text"
-      ></v-text-field>
+      <v-content>
+        <v-text-field
+                v-model="$store.state.filterInput"
+                prepend-icon="search"
+                box
+                solo
+                clearable
+                label="Filter"
+                type="text"
+        ></v-text-field>
 
-      <v-list v-if="$store.state.songs">
-        <template v-for="(song, index) in $store.state.songs">
-          <template v-if="song.title.includes($store.state.filterInput)">
-            <v-list-tile :key="song.id" @click="play(song.id)">
-              <v-list-tile-action>
-                <v-icon v-if="$store.state.currentSong.id === song.id && $store.state.currentStatus.state === 'play'" color="black">
-                  play_circle_outline
-                </v-icon>
-              </v-list-tile-action>
-              <v-list-tile-content>
-                <v-list-tile-title v-text="song.title"></v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
-            <v-divider v-if="index + 1 < $store.state.songs.length" :key="`divider-${index}`"></v-divider>
+        <v-list v-if="$store.state.songs">
+          <template v-for="(song, index) in $store.state.songs">
+            <template v-if="song.title.includes($store.state.filterInput)">
+              <v-list-tile :key="song.id" @click="play(song.id)">
+                <v-list-tile-action>
+                  <v-icon v-if="$store.state.currentSong.id === song.id && $store.state.currentStatus.state === 'play'" color="black">
+                    play_circle_outline
+                  </v-icon>
+                </v-list-tile-action>
+                <v-list-tile-content>
+                  <v-list-tile-title v-text="song.title"></v-list-tile-title>
+                </v-list-tile-content>
+              </v-list-tile>
+              <v-divider v-if="index + 1 < $store.state.songs.length" :key="`divider-${index}`"></v-divider>
+            </template>
           </template>
-        </template>
-      </v-list>
-      <SongInfo v-if="$store.state.currentSong" />
-      <HelloWorld/>
+        </v-list>
+        <SongInfo v-if="$store.state.currentSong" />
+        <HelloWorld/>
+      </v-content>
     </v-app>
   </div>
 </template>
